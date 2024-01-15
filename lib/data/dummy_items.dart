18ec66d64b2b41309ca6f231a9341770a0e3811d
KrishKoria <krishkoria2004@gmail.com>
2024-01-15 23:38:35 +0530

    minor changes

--- lib/data/dummy_items.dart
+++ lib/data/dummy_items.dart
@@ -1,4 +1,5 @@
 import 'package:shopping_list_app/data/categories.dart';
+import 'package:shopping_list_app/models/category.dart';
 import 'package:shopping_list_app/models/grocery_item.dart';
 
 final groceryItems = [
