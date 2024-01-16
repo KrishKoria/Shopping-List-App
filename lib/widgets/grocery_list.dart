51849ce297c9f9a012eb935fe40d33745e05c886
KrishKoria <krishkoria2004@gmail.com>
2024-01-17 01:06:27 +0530

    connected the input form to list screen, tried to use pushNamed only to send data to list screen, made it so that list items are dissmisible and added some text to show when list is empty, removed the dummy items file

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list_app/data/dummy_items.dart';
+import 'package:shopping_list_app/models/grocery_item.dart';
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -9,34 +9,75 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
+  final List<GroceryItem> _groceryItems = [];
+  void _addItem() async {
+    final newItem =
+        await Navigator.of(context).pushNamed('/new-item') as GroceryItem;
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  void _deleteItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text("No Items Added Yet."),
+    );
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (context, index) => Dismissible(
+          onDismissed: (direction) {
+            _deleteItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            title: Text(_groceryItems[index].name),
+            trailing: Text(
+              _groceryItems[index].quantity.toString(),
+            ),
+          ),
+        ),
+      );
+    }
+    // this is how we get the data from the new item screen using named routes
+    // final routeArgs =
+    //     ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+    // if (routeArgs != null) {
+    //   final newItems = [
+    //     GroceryItem(
+    //       id: DateTime.now().toString(),
+    //       name: routeArgs['name'] ?? '',
+    //       quantity: routeArgs['quantity'] ?? 1,
+    //       category: routeArgs['category'] ?? categories[Categories.vegetables]!,
+    //     ),
+    //   ];
+    //   setState(() {
+    //     _groceryItems.addAll(newItems);
+    //   });
+    // }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Grocery List'),
         actions: [
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {
-              Navigator.of(context).pushNamed('/new-item');
-            },
+            onPressed: _addItem,
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (context, index) => ListTile(
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: groceryItems[index].category.color,
-          ),
-          title: Text(groceryItems[index].name),
-          trailing: Text(
-            groceryItems[index].quantity.toString(),
-          ),
-        ),
-      ),
+      body: content,
     );
   }
 }
